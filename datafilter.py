053f5c0ba911b9a270621d49662485d5bb61a76d
macjiong <760783896@qq.com>
2018-04-18 21:15:24 +0100

    add data filter

--- datafilter.py
+++ datafilter.py
@@ -2,14 +2,12 @@ import csv
 
 # , open('output.csv','w') as outputfile
 with open('/Users/pengyuzhou/Desktop/LinkedIn_data_lowercase_no_punctuation.csv', 'r') as csvfile:
-    name = 'mechanical engineer'
+    name = 'software engineer'
     reader = csv.reader(csvfile)
     # writer = csv.DictWriter(outputfile)
-    j = 0
-    have1 = 0
-    donthave = 0
     i = 1
-    writer = csv.writer(open('/Users/pengyuzhou/Desktop/mechanical_engineer_data_lowercase_no_punctuation.csv', 'w'))
+    writer = csv.writer(open('/Users/pengyuzhou/Desktop/software_engineer_lowercase_no_punctuation.csv', 'w'))
+    writer1 = csv.writer(open('/Users/pengyuzhou/Desktop/non_software_engineer_lowercase_no_punctuation.csv', 'w'))
     for row in reader:
         print(i)
         if (row[3].find(name)!=-1):
@@ -26,6 +24,8 @@ with open('/Users/pengyuzhou/Desktop/LinkedIn_data_lowercase_no_punctuation.csv'
             writer.writerow(row)
         elif (row[39].find(name)!=-1):
             writer.writerow(row)
+        else:
+            writer1.writerow(row)
         i = i + 1
 
 csvfile.close()
