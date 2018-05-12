0c7b9a204f46558826d6d16cfd36afc7ac839810
macjiong <760783896@qq.com>
2018-05-12 14:18:20 +0100

    add code

--- content-based.py
+++ content-based.py
@@ -7,6 +7,7 @@ Created on Mon Jan 29 10:20:17 2018
 """
 from __future__ import print_function
 
+
 import sklearn
 
 from sklearn.decomposition import TruncatedSVD
@@ -23,30 +24,35 @@ import csv
 
 v = TfidfVectorizer(min_df=1, stop_words='english')
 episodes = defaultdict(list)
-data = pd.read_csv('C:/Users/ypeng/Dropbox/content-based-test/testdata-content-based.csv')
-# print data['1']
+data = pd.read_csv('/Users/pengyuzhou/Google Drive/content-based-test/testdata-content-based.csv')
+#print data['1']
 data['1'] = data['1'].map(str.lower)
 docs = data['1'].tolist()
 tfidf1 = v.fit_transform(docs)
 tfidf = tfidf1.toarray()
-# feature_names = v.get_feature_names()
-# print (feature_names)
-cosine_similarities = linear_kernel(tfidf[0:1], tfidf).flatten()
-# print (cosine_similarities)
+
+#feature_names = v.get_feature_names()
+#print (feature_names)
+cosine_similarities = linear_kernel(tfidf[0:1],tfidf).flatten()
+cosine_similarities1 = linear_kernel(tfidf[0:1],tfidf[1:2]).flatten()
+
+#print (cosine_similarities)
 # give top n results
 related_docs_indices = cosine_similarities.argsort()[:-500:-1]
 i = 0
+cosine_similarity_value = pd.DataFrame(cosine_similarities, columns = ['cosine_similarity_value']).to_csv('/Users/pengyuzhou/Desktop/software_engineer_cosine_similarity.csv')
 related_doc_num = 0;
 software_num = 501
 for related_docs in related_docs_indices:
-    i = i + 1
-    print("top data top :", i, " is :", related_docs)
-    if (related_docs) < software_num:
-        related_doc_num = related_doc_num + 1
+    i = i+1
+    print("top data top :", i," is :", related_docs)
+    if(related_docs)<software_num:
+        related_doc_num = related_doc_num+1
 
 result = cosine_similarities[related_docs_indices]
-accuracy1 = (float)(related_doc_num - 1) / 500
-print("accuracy: %.30f" % accuracy1)
+accuracy1 = (float)(related_doc_num-1)/500
+print("accuracy: %.30f" %accuracy1)
+
 
 ## LSA
 # lsa = TruncatedSVD(2, algorithm='arpack')
