3baba3865ea782d9b1968662310f4833b10c7428
macjiong <760783896@qq.com>
2018-05-23 16:27:16 +0100

    add algorithms

--- alg_logestic_regression.py
+++ alg_logestic_regression.py
@@ -52,10 +52,51 @@ def logestic_regression():
     #plot the diagram
     precision, recall, _ = precision_recall_curve(Y_test,Y_score)
 
-    plt.step(recall, precision, color='b', alpha=0.2,where='post')
+    # plt.step(recall, precision, color='blue', alpha=0.2,where='post')
+    plt.plot(recall,precision,label = 'logestic regression')
     plt.xlabel('Recall')
     plt.ylabel('Precision')
     plt.ylim([0.0, 1.05])
     plt.xlim([0.0, 1.0])
     plt.title('2-class Precision-Recall curve: AP={0:0.2f}'.format(avg_precesion))
     plt.savefig(globalparameter.folderpath[1] + '/diagram_logestic_regression_p-r_' + globalparameter.jobtitle_path_list[1] + '-extract' + '.png')
+
+
+    X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.3, random_state=0)
+
+    sc = StandardScaler()
+    sc.fit(X_train)
+    X_train_std = sc.transform(X_train)
+    X_test_std = sc.transform(X_test)
+
+    # train logestic regression
+    logreg = linear_model.LogisticRegressionCV()
+    logreg.fit(X_train, Y_train)
+    Y_score = logreg.decision_function(X_test_std)
+
+    # predict
+    prediction = logreg.predict((X_test_std))
+    prepro = logreg.predict_proba(X_test_std)
+    acc = logreg.score(X_test_std, Y_test)
+    avg_precesion = average_precision_score(Y_test, Y_score)
+
+    print('prediction is : {}'.format(prediction))
+    print('-------')
+    print('Logestic regression')
+    print('prepro is : {}'.format(prepro))
+    print('acc is predict proba is {}'.format(acc))
+    print('average precision and recall is {}'.format(avg_precesion))
+
+    # plot the diagram
+    precision, recall, _ = precision_recall_curve(Y_test, Y_score)
+
+    # plt.step(recall, precision, color='pink', alpha=0.2, where='post')
+    plt.plot(recall,precision,label = 'logestic regression CV')
+    plt.xlabel('Recall')
+    plt.ylabel('Precision')
+    plt.ylim([0.0, 1.05])
+    plt.xlim([0.0, 1.0])
+    plt.title('2-class Precision-Recall curve: AP={0:0.2f}'.format(avg_precesion))
+    plt.savefig(
+        globalparameter.folderpath[1] + '/diagram_logestic_regression_p-r_' + globalparameter.jobtitle_path_list[
+            1] + '-extract' + '.png')
--- alg_svm.py
+++ alg_svm.py
@@ -52,12 +52,14 @@ def svm_classification():
     #plot the diagram
     precision, recall, _ = precision_recall_curve(Y_test,Y_score)
 
-    plt.step(recall, precision, color='red', alpha=0.2,where='post')
+    # plt.step(recall, precision, color='red', alpha=0.2,where='post')
+    plt.plot(recall,precision,label = 'SVM LinearSVC')
+
     plt.xlabel('Recall')
     plt.ylabel('Precision')
     plt.ylim([0.0, 1.05])
     plt.xlim([0.0, 1.0])
-    plt.title('2-class Precision-Recall curve: AP={0:0.2f}'.format(avg_precesion))
+    plt.title('2-class Precision-Recall curve')
     plt.savefig(globalparameter.folderpath[1] + '/diagram_svm_p-r_' + globalparameter.jobtitle_path_list[1] + '-extract' + '.png')
 
     X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.3, random_state=0)
@@ -88,11 +90,52 @@ def svm_classification():
     # plot the diagram
     precision, recall, _ = precision_recall_curve(Y_test, Y_score)
 
-    plt.step(recall, precision, color='red', alpha=0.2, where='post')
+    # plt.step(recall, precision, color='green', alpha=0.2, where='post')
+    plt.plot(recall,precision,label = 'SVM NuSVC')
+
     plt.xlabel('Recall')
     plt.ylabel('Precision')
     plt.ylim([0.0, 1.05])
     plt.xlim([0.0, 1.0])
-    plt.title('2-class Precision-Recall curve: AP={0:0.2f}'.format(avg_precesion))
+    plt.title('2-class Precision-Recall curve')
+    plt.savefig(globalparameter.folderpath[1] + '/diagram_svm_p-r_' + globalparameter.jobtitle_path_list[
+        1] + '-extract' + '.png')
+
+    X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.3, random_state=0)
+
+    sc = StandardScaler()
+    sc.fit(X_train)
+    X_train_std = sc.transform(X_train)
+    X_test_std = sc.transform(X_test)
+
+    # train logestic regression
+    svm_classifier = svm.SVC()
+    svm_classifier.fit(X_train, Y_train)
+    Y_score = svm_classifier.decision_function(X_test_std)
+
+    # predict
+    prediction = svm_classifier.predict((X_test_std))
+    # prepro = svm_classifier.predict_proba(X_test_std)
+    acc = svm_classifier.score(X_test_std, Y_test)
+    avg_precesion = average_precision_score(Y_test, Y_score)
+
+    print('prediction is : {}'.format(prediction))
+    print('-------')
+    print('SVM classification')
+    # print('prepro is : {}'.format(prepro))
+    print('acc is predict proba is {}'.format(acc))
+    print('average precision and recall is {}'.format(avg_precesion))
+
+    # plot the diagram
+    precision, recall, _ = precision_recall_curve(Y_test, Y_score)
+
+    # plt.step(recall, precision, color='black', alpha=0.2, where='post')
+    plt.plot(recall,precision,label = 'SVM SVC')
+
+    plt.xlabel('Recall')
+    plt.ylabel('Precision')
+    plt.ylim([0.0, 1.05])
+    plt.xlim([0.0, 1.0])
+    plt.title('2-class Precision-Recall curve')
     plt.savefig(globalparameter.folderpath[1] + '/diagram_svm_p-r_' + globalparameter.jobtitle_path_list[
         1] + '-extract' + '.png')
