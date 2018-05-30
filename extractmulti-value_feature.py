06211fb85e7ed29518492d51c8d8fd1c0b7ccc61
macjiong <760783896@qq.com>
2018-05-30 20:22:21 +0100

    add code for extract multiple value of feature

---
+++ extractmulti-value_feature.py
@@ -0,0 +1,121 @@
+import pandas as pd
+import numpy as np
+def extractskill(folderpath,job_title_name):
+    user_data = pd.DataFrame(pd.read_csv('/Users/pengyuzhou/Google Drive/Linkedin_datafile/software_engineer/software_engineer_lowercase_no_punctuation.csv'))
+    df = user_data.iloc[:,[65]].values.tolist()
+    user_skill_data = []
+    user_skill_data1 = []
+    user_skill_data_for_dummy = pd.DataFrame()
+    # print(df)
+    for i in range(len(df)):
+        user_skill_data.append(' '.join(str(k) for k in df[i]))
+    # print(user_skill_data)
+
+    for i in range(len(user_skill_data)):
+        user_skill_data1.append(user_skill_data[i].split())
+    # print(user_skill_data1)
+
+    user_skill_data1 = pd.DataFrame({'skills':user_skill_data1})
+    # print(user_skill_data1)
+    length1 = len(user_skill_data1)
+
+    i=0
+    for row in user_skill_data1.iterrows():
+        user_skill_data_for_dummy['user'+str(i)] = row[1]
+        i = i+1
+
+
+    print(user_skill_data_for_dummy)
+    skill_variable_array = pd.get_dummies(pd.DataFrame(user_skill_data1.skills.values.tolist()))
+    print(skill_variable_array.shape)
+
+
+def extractschool(folderpath):
+    user_data = pd.DataFrame(pd.read_csv(
+        '/Users/pengyuzhou/Google Drive/Linkedin_datafile/software_engineer/software_engineer_lowercase_no_punctuation.csv'))
+    df = user_data.iloc[:, [45]].values.tolist()
+    user_school_data = []
+    user_school_data1 = []
+    user_school_data_for_dummy = pd.DataFrame()
+    # print(df)
+    for i in range(len(df)):
+        user_school_data.append(' '.join(str(k) for k in df[i]))
+    # print(user_skill_data)
+
+    for i in range(len(user_school_data)):
+        user_school_data1.append(user_school_data[i].split())
+    # print(user_skill_data1)
+
+    user_school_data1 = pd.DataFrame({'skills': user_school_data1})
+    # print(user_skill_data1)
+    length1 = len(user_school_data1)
+
+    i = 0
+    for row in user_school_data1.iterrows():
+        user_school_data_for_dummy['user' + str(i)] = row[1]
+        i = i + 1
+
+    print(user_school_data_for_dummy)
+    skill_variable_array = pd.get_dummies(pd.DataFrame(user_school_data1.skills.values.tolist()))
+    print(skill_variable_array.shape)
+
+
+def extractmajor(folderpath):
+    user_data = pd.DataFrame(pd.read_csv(
+        '/Users/pengyuzhou/Google Drive/Linkedin_datafile/software_engineer/software_engineer_lowercase_no_punctuation.csv'))
+    df = user_data.iloc[:, [47]].values.tolist()
+    user_major_data = []
+    user_major_data1 = []
+    user_major_data_for_dummy = pd.DataFrame()
+    # print(df)
+    for i in range(len(df)):
+        user_major_data.append(' '.join(str(k) for k in df[i]))
+    # print(user_skill_data)
+
+    for i in range(len(user_major_data)):
+        user_major_data1.append(user_major_data[i].split())
+    # print(user_skill_data1)
+
+    user_major_data1 = pd.DataFrame({'skills': user_major_data1})
+    # print(user_skill_data1)
+    length1 = len(user_major_data1)
+
+    i = 0
+    for row in user_major_data1.iterrows():
+        user_major_data_for_dummy['user' + str(i)] = row[1]
+        i = i + 1
+
+    print(user_major_data_for_dummy)
+    skill_variable_array = pd.get_dummies(pd.DataFrame(user_major_data1.skills.values.tolist()))
+    print(skill_variable_array.shape)
+
+
+def extractworkcompany(folderpath):
+#now work company index = [4], past work company index = [10,16,22,28,34,40]
+    user_data = pd.DataFrame(pd.read_csv(
+        '/Users/pengyuzhou/Google Drive/Linkedin_datafile/software_engineer/software_engineer_lowercase_no_punctuation.csv'))
+    df = user_data.iloc[:, [10]].values.tolist()
+    user_workcompany_data = []
+    user_workcompany_data1 = []
+    user_workcompany_data_for_dummy = pd.DataFrame()
+    # print(df)
+    for i in range(len(df)):
+        user_workcompany_data.append(' '.join(str(k) for k in df[i]))
+    # print(user_skill_data)
+
+    for i in range(len(user_workcompany_data)):
+        user_workcompany_data1.append(user_workcompany_data[i].split())
+    # print(user_skill_data1)
+
+    user_workcompany_data1 = pd.DataFrame({'skills': user_workcompany_data1})
+    # print(user_skill_data1)
+    length1 = len(user_workcompany_data1)
+
+    i = 0
+    for row in user_workcompany_data1.iterrows():
+        user_workcompany_data_for_dummy['user' + str(i)] = row[1]
+        i = i + 1
+
+    print(user_workcompany_data_for_dummy)
+    skill_variable_array = pd.get_dummies(pd.DataFrame(user_workcompany_data1.skills.values.tolist()))
+    print(skill_variable_array.shape)
